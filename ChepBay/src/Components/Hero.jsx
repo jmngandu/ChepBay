09ad1fd2e8a5971cb0e1f2ca78c85794332f42e8
jmngandu <jamesmngandu@gmail.com>
2024-02-24 12:21:22 +0300

    commit

--- ChepBay/src/Components/Hero.jsx
+++ ChepBay/src/Components/Hero.jsx
@@ -19,13 +19,13 @@ export default function Hero(props){
                <img src="/src/assets/homeimage.png" className="home-img"/>
                <h1>You ask we deliver</h1>
             </div>
-            {/* <div className="about-section">
+            <div className="about-section">
                 <h1>ABOUT US!</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fuga perferendis architecto veritatis eveniet dignissimos voluptates dicta nobis, eligendi eaque quia illum quos! Tempora aspernatur expedita commodi sed quas omnis?</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat pariatur molestiae consequatur, atque laudantium eveniet excepturi quisquam deserunt ipsa facere aspernatur ex maxime. Impedit harum vel corrupti quisquam voluptate.</p>
-            </div> */}
+            </div>
             <div className="Services-section">
-                <h1>SERVICE WE OFFER!</h1>
+                <h1>SERVICES WE OFFER!</h1>
                 <div className="service-area">
                 {dataSet}
                 </div>
