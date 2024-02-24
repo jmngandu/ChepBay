87585582b03d8e6dede195fbb30c76db60dc5896
jmngandu <jamesmngandu@gmail.com>
2024-02-24 12:56:53 +0300

    commi

--- ChepBay/src/Service.jsx
+++ ChepBay/src/Service.jsx
@@ -5,7 +5,7 @@ export default function Service(props){
     <>
     <div className="service-card">
        <img src={`src/assets/images/${props.displayImg}`} className="service-image" />
-       <p className="service-title">{props.title}</p>
+       <p className="service-title"><span className="bold boldl">{props.title}</span></p>
        <p className="description">{props.description}</p>
        <p className="price"><span className="bold">From ${props.price}</span> / Item</p>
        </div>
