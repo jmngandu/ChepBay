1c189957f102f1d77d65f3f5fceb4ad26906dd24
jmngandu <jamesmngandu@gmail.com>
2024-09-30 22:26:17 +0300

    chepbay

---
+++ chepbay/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
+import Service from "./Service";
+import data from "./Components/data";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ chepbay/src/Components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+export default function Nav() {
+  return (
+    <>
+      <nav className="nav-section">
+        <div className="nav-top">
+          <h3 className="logo">ChepBay</h3>
+          <input typeof="search" placeholder="Login" />
+        </div>
+        <div className="nav-bottom">
+          <ul className="nav-list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>News</li>
+            <li>Contact</li>
+          </ul>
+          <h4>Cart</h4>
+        </div>
+      </nav>
+    </>
+  );
+}
